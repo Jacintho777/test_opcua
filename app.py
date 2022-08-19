2251b4ff564d1f2e9d8167393e8f5c3db0d89afa
Jacintho777 <mpeteyemawuli@gmail.com>
2022-08-19 03:33:12 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from dash import Dash,html,dcc
+from dash.dependencies import Input, Output
+from client_prod_simul_test import update_plots
+
+app = Dash(__name__)
+
+server = app.server
+
+app.layout = html.Div(
+            children = [
+                dcc.Interval(id = 'grph_interval',interval = 200, n_intervals=0),
+                html.H1('OPC-UA appTest',style={'text-align':'center'}),
+                dcc.Graph(id = 'grph',style = {'margin-left':'20rem','margin-right':'20rem'}),
+            ]
+)
+
+@app.callback(
+    Output('grph','figure'),
+
+    Input('grph_interval','n_intervals'),
+)
+
+def update(n_intervals):
+    
+    return update_plots()
+
+if __name__ == '__main__':
+
+    app.run_server(debug = True)
---
+++ client_prod_simul_test.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from opcua import Client
+
+
+url = "opc.tcp://192.168.11.104:4840"
+client = Client(url)
+
+try:
+    client.connect()
+    print("Client connected")
+except:
+    print('Connexion failed !')
+
+nb_prod = 0
+cycle_time,cycle_time_dur = datetime.now(),0.0
+
+temp_values,cycle_time_values,energy_values = [],[],[]
+
+def update_values() :
+
+    global cycle_time,nb_prod,cycle_time_dur
+
+    Temp = client.get_node("ns= 2; i= 3")
+    Temperature = Temp.get_value()
+
+    Step_3 = client.get_node("ns= 2; i= 6")
+    step_3 = Step_3.get_value()
+
+    Energy = client.get_node("ns= 2; i= 7")
+    energy_cons = Energy.get_value()
+
+    if step_3 == True :
+
+        nb_prod += 1
+        cycle_time_dur = (datetime.now()-cycle_time).total_seconds()
+        cycle_time = datetime.now()
+
+    print(Temperature, cycle_time_dur, energy_cons)
+
+    return Temperature,cycle_time_dur*100,energy_cons/100
+
+def update_plots(): #function to call repeatedly to update the Graph object in the dashboard
+
+    global temp_values,cycle_time_values,energy_values
+
+    temp,cy_time,energy = update_values()
+
+    temp_values.append(temp)
+    cycle_time_values.append(cy_time)
+    energy_values.append(energy)
+
+    abscisses = list(range(len(temp_values)))
+
+    fig = {
+        'data': [
+            {'x': abscisses, 'y': temp_values,'name' : 'Temperature'},
+            {'x': abscisses, 'y': cycle_time_values,'name' : 'Cycle time'},
+            {'x': abscisses, 'y': energy_values,'name' : 'Energy consumed'},
+        ],
+        'layout': {
+            'title': 'Molding machine state variables visualization'
+        }
+    }
+
+    return fig
+    
